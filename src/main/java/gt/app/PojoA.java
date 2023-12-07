4d86d87c5d9c3bec764df1c86a1c9ee2a1cc273b
Ganesh Tiwari(gtiwari333) <ganesh.tiwari@shutterfly.com>
2023-12-07 14:20:37 -0600

    added back

--- src/main/java/gt/app/PojoA.java
+++ src/main/java/gt/app/PojoA.java
@@ -2,15 +2,21 @@ package gt.app;
 
 import lombok.Data;
 
+import javax.annotation.Nullable;
+
 @Data
 public class PojoA {
+    @Nullable
     String firstName;
+    @Nullable
     String lastName;
     int age;
+    @Nullable
     String country;
+    @Nullable
     String language;
 
-    public PojoA(String firstName, int age) {
+    public PojoA(@Nullable String firstName, int age) {
         this.firstName = firstName;
         this.age = age;
     }
